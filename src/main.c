e6321ae721bcf4dfb7dcd77248d367a067bd2592
Bruno Andrade <brungomes@msn.com>
2025-12-06 17:20:04 -0300

    printf de comando não encontrado

--- src/main.c
+++ src/main.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+
+  if (argc < 2)
+    return 1;
+  
+  printf("%s: command not found", argv[2]);
+
   // Flush after every printf
   setbuf(stdout, NULL);
 
