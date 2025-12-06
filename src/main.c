ded7d83faf19e26ef1f3e84279902c06dd2993ab
Bruno Andrade <brungomes@msn.com>
2025-12-06 17:52:55 -0300

    Implementado REPL

--- src/main.c
+++ src/main.c
@@ -3,22 +3,25 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-
-  // Flush after every printf
-  setbuf(stdout, NULL);
-
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
-
+  
   char command[256];
-  if (fgets(command, sizeof(command), stdin) != NULL){
-    //Usa strcspn para localizar a primeira ocorrência de \n e então substiuimos por \0, definindo assim o novo tamanho da string
-    command[strcspn(command, "\n")] = '\0';
+  
+  //REPL (Read-Eval-Print Loop).
+  do{
+    printf("$ ");
+    
+    // Flush after every print
+    setbuf(stdout, NULL);
 
-    if (strlen(command) > 0){
-      printf("%s: command not found", command);
-    }
-  };
+    if (fgets(command, sizeof(command), stdin) != NULL){
+      //Usa strcspn para localizar a primeira ocorrência de \n e então substiuimos por \0, definindo assim o novo tamanho da string
+      command[strcspn(command, "\n")] = '\0';
+
+      if (strlen(command) > 0){
+        printf("%s: command not found\n", command);
+      }
+    };
+  } while(1);
 
   return 0;
 }
